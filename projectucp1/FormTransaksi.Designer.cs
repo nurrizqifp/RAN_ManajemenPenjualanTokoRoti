620668939efe7c61fc584d3e3fc7c3cccd31bbcf
nurrizqifp <nurrizqifebriansyah@gmail.com>
2026-04-15 12:01:07 +0700

    Form Designer untuk transaksi

--- projectucp1/FormTransaksi.Designer.cs
+++ projectucp1/FormTransaksi.Designer.cs
@@ -23,53 +23,100 @@
             this.txtBayar = new System.Windows.Forms.TextBox();
             this.lblKembalian = new System.Windows.Forms.Label();
             this.btnSimpan = new System.Windows.Forms.Button();
-
             ((System.ComponentModel.ISupportInitialize)(this.dgvKeranjang)).BeginInit();
             this.SuspendLayout();
-
-            cmbProduk.Location = new System.Drawing.Point(30, 30);
-            cmbProduk.SelectedIndexChanged += new System.EventHandler(this.cmbProduk_SelectedIndexChanged);
-
-            txtHarga.Location = new System.Drawing.Point(200, 30);
-            txtHarga.ReadOnly = true;
-
-            txtQty.Location = new System.Drawing.Point(350, 30);
-
-            btnTambah.Location = new System.Drawing.Point(500, 30);
-            btnTambah.Text = "Tambah";
-            btnTambah.Click += new System.EventHandler(this.btnTambah_Click);
-
-            dgvKeranjang.Location = new System.Drawing.Point(30, 80);
-            dgvKeranjang.Size = new System.Drawing.Size(600, 200);
-
-            lblTotal.Location = new System.Drawing.Point(30, 300);
-            lblTotal.Text = "Total: 0";
-
-            txtBayar.Location = new System.Drawing.Point(30, 330);
-            txtBayar.TextChanged += new System.EventHandler(this.txtBayar_TextChanged);
-
-            lblKembalian.Location = new System.Drawing.Point(30, 360);
-            lblKembalian.Text = "Kembalian: 0";
-
-            btnSimpan.Location = new System.Drawing.Point(30, 400);
-            btnSimpan.Text = "Simpan";
-            btnSimpan.Click += new System.EventHandler(this.btnSimpan_Click);
-
-            this.Controls.Add(cmbProduk);
-            this.Controls.Add(txtHarga);
-            this.Controls.Add(txtQty);
-            this.Controls.Add(btnTambah);
-            this.Controls.Add(dgvKeranjang);
-            this.Controls.Add(lblTotal);
-            this.Controls.Add(txtBayar);
-            this.Controls.Add(lblKembalian);
-            this.Controls.Add(btnSimpan);
-
+            // 
+            // cmbProduk
+            // 
+            this.cmbProduk.Location = new System.Drawing.Point(30, 30);
+            this.cmbProduk.Name = "cmbProduk";
+            this.cmbProduk.Size = new System.Drawing.Size(121, 21);
+            this.cmbProduk.TabIndex = 0;
+            this.cmbProduk.SelectedIndexChanged += new System.EventHandler(this.cmbProduk_SelectedIndexChanged);
+            // 
+            // txtHarga
+            // 
+            this.txtHarga.Location = new System.Drawing.Point(200, 30);
+            this.txtHarga.Name = "txtHarga";
+            this.txtHarga.ReadOnly = true;
+            this.txtHarga.Size = new System.Drawing.Size(100, 20);
+            this.txtHarga.TabIndex = 1;
+            // 
+            // txtQty
+            // 
+            this.txtQty.Location = new System.Drawing.Point(350, 30);
+            this.txtQty.Name = "txtQty";
+            this.txtQty.Size = new System.Drawing.Size(100, 20);
+            this.txtQty.TabIndex = 2;
+            // 
+            // btnTambah
+            // 
+            this.btnTambah.Location = new System.Drawing.Point(500, 30);
+            this.btnTambah.Name = "btnTambah";
+            this.btnTambah.Size = new System.Drawing.Size(75, 23);
+            this.btnTambah.TabIndex = 3;
+            this.btnTambah.Text = "Tambah";
+            this.btnTambah.Click += new System.EventHandler(this.btnTambah_Click);
+            // 
+            // dgvKeranjang
+            // 
+            this.dgvKeranjang.Location = new System.Drawing.Point(30, 80);
+            this.dgvKeranjang.Name = "dgvKeranjang";
+            this.dgvKeranjang.Size = new System.Drawing.Size(600, 200);
+            this.dgvKeranjang.TabIndex = 4;
+            // 
+            // lblTotal
+            // 
+            this.lblTotal.Location = new System.Drawing.Point(30, 300);
+            this.lblTotal.Name = "lblTotal";
+            this.lblTotal.Size = new System.Drawing.Size(100, 23);
+            this.lblTotal.TabIndex = 5;
+            this.lblTotal.Text = "Total: 0";
+            // 
+            // txtBayar
+            // 
+            this.txtBayar.Location = new System.Drawing.Point(30, 330);
+            this.txtBayar.Name = "txtBayar";
+            this.txtBayar.Size = new System.Drawing.Size(100, 20);
+            this.txtBayar.TabIndex = 6;
+            this.txtBayar.TextChanged += new System.EventHandler(this.txtBayar_TextChanged);
+            // 
+            // lblKembalian
+            // 
+            this.lblKembalian.Location = new System.Drawing.Point(30, 360);
+            this.lblKembalian.Name = "lblKembalian";
+            this.lblKembalian.Size = new System.Drawing.Size(100, 23);
+            this.lblKembalian.TabIndex = 7;
+            this.lblKembalian.Text = "Kembalian: 0";
+            // 
+            // btnSimpan
+            // 
+            this.btnSimpan.Location = new System.Drawing.Point(30, 400);
+            this.btnSimpan.Name = "btnSimpan";
+            this.btnSimpan.Size = new System.Drawing.Size(75, 23);
+            this.btnSimpan.TabIndex = 8;
+            this.btnSimpan.Text = "Simpan";
+            this.btnSimpan.Click += new System.EventHandler(this.btnSimpan_Click);
+            // 
+            // FormTransaksi
+            // 
+            this.ClientSize = new System.Drawing.Size(683, 451);
+            this.Controls.Add(this.cmbProduk);
+            this.Controls.Add(this.txtHarga);
+            this.Controls.Add(this.txtQty);
+            this.Controls.Add(this.btnTambah);
+            this.Controls.Add(this.dgvKeranjang);
+            this.Controls.Add(this.lblTotal);
+            this.Controls.Add(this.txtBayar);
+            this.Controls.Add(this.lblKembalian);
+            this.Controls.Add(this.btnSimpan);
+            this.Name = "FormTransaksi";
             this.Text = "FormTransaksi";
             this.Load += new System.EventHandler(this.FormTransaksi_Load);
-
             ((System.ComponentModel.ISupportInitialize)(this.dgvKeranjang)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         private System.Windows.Forms.ComboBox cmbProduk;
