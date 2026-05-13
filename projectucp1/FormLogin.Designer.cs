dee1e1cbc29b2ff586edb39e6d564e5b26c511d6
nurrizqifp <nurrizqifebriansyah@gmail.com>
2026-05-13 11:37:01 +0700

    Add designer code for Windows Forms login form
    Added FormLogin.Designer.cs with layout and initialization for the login form in projectucp1. Includes username and password fields, labels, login button, and event handler connections for form events and button clicks.

---
+++ projectucp1/FormLogin.Designer.cs
@@ -0,0 +1,135 @@
+﻿namespace projectucp1
+{
+    partial class FormLogin
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.BtnLogin = new System.Windows.Forms.Button();
+            this.TxtBoxUsername = new System.Windows.Forms.TextBox();
+            this.TxtBoxPassword = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.LblWelcome = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // BtnLogin
+            // 
+            this.BtnLogin.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnLogin.Location = new System.Drawing.Point(196, 201);
+            this.BtnLogin.Margin = new System.Windows.Forms.Padding(2);
+            this.BtnLogin.Name = "BtnLogin";
+            this.BtnLogin.Size = new System.Drawing.Size(102, 31);
+            this.BtnLogin.TabIndex = 0;
+            this.BtnLogin.Text = "Login";
+            this.BtnLogin.UseVisualStyleBackColor = true;
+            this.BtnLogin.Click += new System.EventHandler(this.BtnLogin_Click);
+            // 
+            // TxtBoxUsername
+            // 
+            this.TxtBoxUsername.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TxtBoxUsername.Location = new System.Drawing.Point(225, 100);
+            this.TxtBoxUsername.Margin = new System.Windows.Forms.Padding(2);
+            this.TxtBoxUsername.Name = "TxtBoxUsername";
+            this.TxtBoxUsername.Size = new System.Drawing.Size(152, 24);
+            this.TxtBoxUsername.TabIndex = 1;
+            // 
+            // TxtBoxPassword
+            // 
+            this.TxtBoxPassword.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TxtBoxPassword.Location = new System.Drawing.Point(225, 141);
+            this.TxtBoxPassword.Margin = new System.Windows.Forms.Padding(2);
+            this.TxtBoxPassword.Name = "TxtBoxPassword";
+            this.TxtBoxPassword.PasswordChar = '*';
+            this.TxtBoxPassword.Size = new System.Drawing.Size(152, 24);
+            this.TxtBoxPassword.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(111, 100);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(77, 18);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Username";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(111, 141);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(75, 18);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Password";
+            // 
+            // LblWelcome
+            // 
+            this.LblWelcome.AutoSize = true;
+            this.LblWelcome.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LblWelcome.Location = new System.Drawing.Point(178, 45);
+            this.LblWelcome.Name = "LblWelcome";
+            this.LblWelcome.Size = new System.Drawing.Size(168, 25);
+            this.LblWelcome.TabIndex = 5;
+            this.LblWelcome.Text = "Silahkan Login";
+            this.LblWelcome.Click += new System.EventHandler(this.LblWelcome_Click);
+            // 
+            // FormLogin
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(533, 292);
+            this.Controls.Add(this.LblWelcome);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.TxtBoxPassword);
+            this.Controls.Add(this.TxtBoxUsername);
+            this.Controls.Add(this.BtnLogin);
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "FormLogin";
+            this.Text = "Login";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.FormLogin_FormClosing);
+            this.Load += new System.EventHandler(this.FormLogin_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button BtnLogin;
+        private System.Windows.Forms.TextBox TxtBoxUsername;
+        private System.Windows.Forms.TextBox TxtBoxPassword;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label LblWelcome;
+    }
+}
+
